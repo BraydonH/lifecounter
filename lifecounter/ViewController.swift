5eda4d9a4cdfc2f1243c74520ac419fb82b4ecfd
cse-loaner <cse-loaner@d-10-19-101-161.dhcp4.washington.edu>
2019-01-29 15:51:05 -0800

    Initial Commit

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by cse-loaner on 1/29/19.
+//  Copyright © 2019 uw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
